57174ef7c9e70fd0b2fc8d9b6f175bef8645951b
Scott <102482627+sdgoldstein@users.noreply.github.com>
2024-12-27 19:45:24 -0800

    Removing Hover on Cancel Button

--- src/main/components/core/modal/CancelButton.svelte
+++ src/main/components/core/modal/CancelButton.svelte
@@ -1,10 +1,7 @@
-
-
 <script module lang="ts">
     import { type BaseComponentProps } from "../../component.js";
 
-    export interface CancelButtonProps
-        extends BaseComponentProps {
+    export interface CancelButtonProps extends BaseComponentProps {
         oncancel: (event: MouseEvent) => void;
     }
 </script>
@@ -12,8 +9,21 @@
 <script lang="ts">
     import Button from "../Button.svelte";
 
-    let { id, testid, colorVariant="surface-dark", dynamicColorTheme, oncancel }: CancelButtonProps = $props();
-
+    let {
+        id,
+        testid,
+        colorVariant = "surface-dark",
+        dynamicColorTheme,
+        oncancel,
+    }: CancelButtonProps = $props();
 </script>
 
-<Button {id} {testid} {colorVariant} {dynamicColorTheme} classOverride="hover:bg-surface-darkest bg-surface-dark border-surface-darkest border" onclick={oncancel}>Cancel</Button>
+<!-- I use to have hover:bg-surface-darkest, but we don't have hover on the submit buttong, so removing it to be consistent-->
+<Button
+    {id}
+    {testid}
+    {colorVariant}
+    {dynamicColorTheme}
+    classOverride="bg-surface-dark border-surface-darkest border"
+    onclick={oncancel}>Cancel</Button
+>
