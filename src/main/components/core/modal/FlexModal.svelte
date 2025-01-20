815c884e6a36fb853f6dd6d736a92d3b573accf8
Scott <102482627+sdgoldstein@users.noreply.github.com>
2025-01-19 20:31:13 -0800

    Setting minium width to 30%

--- src/main/components/core/modal/FlexModal.svelte
+++ src/main/components/core/modal/FlexModal.svelte
@@ -1,34 +1,38 @@
-
-
 <script lang="ts">
-    import { Dialog as DialogPrimitive} from "bits-ui";
-    import {buildTestId, type ParentComponentProps} from "../../component.js"
+    import { Dialog as DialogPrimitive } from "bits-ui";
+    import { buildTestId, type ParentComponentProps } from "../../component.js";
     import type { Snippet } from "svelte";
     import { X } from "lucide-svelte";
 
     interface FlexModalProps extends ParentComponentProps {
         open: boolean;
-        header?:Snippet<[string|undefined, string|undefined]>;
-        footer?:Snippet<[string|undefined, string|undefined]>;
+        header?: Snippet<[string | undefined, string | undefined]>;
+        footer?: Snippet<[string | undefined, string | undefined]>;
     }
 
     let {
         id,
-        testid:testidProp, 
+        testid: testidProp,
         open = $bindable(),
         header,
-        children:providedChildren,
+        children: providedChildren,
         footer,
     }: FlexModalProps = $props();
 
-    let testId:string|undefined=$derived(buildTestId(id, testidProp));
+    let testId: string | undefined = $derived(buildTestId(id, testidProp));
 </script>
 
 <DialogPrimitive.Root bind:open>
     <DialogPrimitive.Trigger />
     <DialogPrimitive.Portal>
-        <DialogPrimitive.Overlay class="fixed inset-0 z-50 bg-overlay/50 w-full h-full"/>
-        <DialogPrimitive.Content {id} data-testid={testId} class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 max-w-[94%] rounded-lg bg-surface px-8 py-2">
+        <DialogPrimitive.Overlay
+            class="fixed inset-0 z-50 bg-overlay/50 w-full h-full"
+        />
+        <DialogPrimitive.Content
+            {id}
+            data-testid={testId}
+            class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 max-w-[94%] min-w-[30%] rounded-lg bg-surface px-8 py-2"
+        >
             <div class="flex flex-col">
                 <div class="w-full">
                     {@render header?.(id, testId)}
@@ -40,8 +44,10 @@
                     {@render footer?.(id, testId)}
                 </div>
             </div>
-            <DialogPrimitive.Close class="absolute right-2 top-2 opacity-70 hover:opacity-100">
-                <X size=16 />
+            <DialogPrimitive.Close
+                class="absolute right-2 top-2 opacity-70 hover:opacity-100"
+            >
+                <X size="16" />
             </DialogPrimitive.Close>
         </DialogPrimitive.Content>
     </DialogPrimitive.Portal>
