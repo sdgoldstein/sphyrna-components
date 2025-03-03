ee9ba0ea0ec10c9e09e6a01e9f76c99aee9bbf58
Scott <102482627+sdgoldstein@users.noreply.github.com>
2025-03-02 16:54:32 -0800

    Adding export for datatable types

--- src/main/components/data/index.ts
+++ src/main/components/data/index.ts
@@ -1,6 +1,7 @@
 
 import Datatable from "./Datatable/Datatable.svelte";
 import type {DatatableProps} from "./Datatable/datatable";
+import type {ColumnDefinition, DataRow, TableDataSource} from "./Datatable/datatableModel";
 
 export {Datatable};
-export type{DatatableProps};
+export type{DatatableProps, TableDataSource, ColumnDefinition, DataRow};
